d7ccd287d62821b5c2ff11cf93fd0ee9d1655ae2
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-05-15 12:23:46 +0300

    dlt files

--- src/components/Checkbox/checkbox.types.ts
+++
@@ -1,7 +0,0 @@
-export interface CheckboxProps extends Omit<
-  React.InputHTMLAttributes<HTMLInputElement>,
-  "type"
-> {
-  label?: string;
-  invalid?: boolean;
-}
--- src/components/FormField/FormField.types.ts
+++
@@ -1,7 +0,0 @@
-export interface FormFieldProps {
-  label?: string;
-  error?: string;
-  hint?: string;
-  required?: boolean;
-  children: React.ReactNode;
-}
