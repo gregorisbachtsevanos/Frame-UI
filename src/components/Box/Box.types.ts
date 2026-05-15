6db16482947fd42e0298c29e75e16d1be59e436d
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-05-15 12:24:02 +0300

    dlt files

--- src/components/Box/Box.types.ts
+++
@@ -1,7 +0,0 @@
-import type { CSSProperties, ElementType, HTMLAttributes } from "react";
-
-export interface BoxProps extends HTMLAttributes<HTMLElement> {
-  as?: ElementType;
-  className?: string;
-  style?: CSSProperties;
-}
