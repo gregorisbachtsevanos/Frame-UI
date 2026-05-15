6162a4ed72947f52cb7d869a42533c9b887c8c79
gbachtse <grigorios.bachtsevanos@netcompany.com>
2026-05-15 12:24:26 +0300

    dlt files

--- src/components/Label/Label.tsx
+++
@@ -1,7 +0,0 @@
-import { cn } from "../../utils/cn";
-import { label } from "./Label.css";
-import type { LabelProps } from "./Label.types";
-
-export const Label = ({ className, ...props }: LabelProps) => {
-  return <label className={cn(label, className)} {...props} />;
-};
--- src/components/Label/Label.types.ts
+++
@@ -1,6 +0,0 @@
-import type { CSSProperties, LabelHTMLAttributes } from "react";
-
-export interface LabelProps extends LabelHTMLAttributes<HTMLLabelElement> {
-  className?: string;
-  style?: CSSProperties;
-}
